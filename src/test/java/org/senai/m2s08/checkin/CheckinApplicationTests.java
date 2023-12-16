770fe2a697616ce5eac6dce2a502632896cff0a3
goptun <ramos.matheus@icloud.com>
2023-12-16 10:27:56 -0300

    [M02S08] Ex 01 - Mini-Projeto Checkin - Criação do projeto

---
+++ src/test/java/org/senai/m2s08/checkin/CheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senai.m2s08.checkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
